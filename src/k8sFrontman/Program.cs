9e812b2c5276c474d47acfe6ce8691cafa31f427
Patrick Evers <pmdevers@gmail.com>
2026-02-12 10:48:14 +0100

    Adds default deployment and service creation
    Enables automatic creation of a default deployment and service
    during operator installation.
    
    This simplifies initial setup and ensures a basic functioning
    instance after installation.
    
    Updates k8sOperator package to the latest alpha version.

--- src/k8sFrontman/Program.cs
+++ src/k8sFrontman/Program.cs
@@ -1,6 +1,8 @@
+using k8s.Frontman.Features.Install;
 using k8s.Frontman.Features.Providers;
 using k8s.Frontman.Features.Releases;
 using k8s.Operator;
+using k8s.Operator.Generation;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -23,7 +25,8 @@ builder.Services.AddOperator(x =>
     x.Operator.Namespace = "default";
     x.Operator.ContainerRegistry = "ghcr.io";
     x.Operator.ContainerRepository = "pmdevers/k8s-frontman";
-    x.Operator.ContainerTag = "v1.0.0";
+    x.WithDeployment();
+    x.WithService();
 });
 
 var app = builder.Build();
